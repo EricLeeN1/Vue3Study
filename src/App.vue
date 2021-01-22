c6684c68cb54c645f7686e6ea4eab8e742199b62
EricLeeN1 <179310009@qq.com>
2021-01-22 17:34:41 +0800

    createApp

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+<img alt="Vue logo" src="./assets/logo.png" />
+<HelloWorld :msg="msg" />
+<button @click="reverseMessage">反转 Message</button>
+<input type="text" v-model="msg">
+<ol>
+    <ToDo v-for="(item,index) in todos" :todo="item" :key="index" :data-id="index" />
+</ol>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+            msg: 'Hello Vue 3.0 + Vite',
+            todos: [{
+                    text: 'Learn JavaScript'
+                },
+                {
+                    text: 'Learn Vue'
+                },
+                {
+                    text: 'Build something awesome'
+                }
+            ]
+        }
+    },
+    components: {
+        HelloWorld,
+        ToDo
+    },
+    methods: {
+        reverseMessage() {
+            this.msg = this.msg
+                .split('')
+                .reverse()
+                .join('')
+        }
+    }
 }
 </script>
