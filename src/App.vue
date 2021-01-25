7365e821a3d7478dfc81278bb308ffdbf91bc1c2
EricLeeN1 <179310009@qq.com>
2021-01-25 11:23:30 +0800

    代码更新

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,30 @@
 <template>
-<img alt="Vue logo" src="./assets/logo.png" />
-<HelloWorld :msg="msg" />
-<button @click="reverseMessage">反转 Message</button>
-<input type="text" v-model="msg">
-<ol>
-    <ToDo v-for="(item,index) in todos" :todo="item" :key="index" :data-id="index" />
-</ol>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import ToDo from './components/ToDo.vue'
-
-export default {
-    name: 'App',
-    data() {
-        return {
-            msg: 'Hello Vue 3.0 + Vite',
-            todos: [{
-                    text: 'Learn JavaScript'
-                },
-                {
-                    text: 'Learn Vue'
-                },
-                {
-                    text: 'Build something awesome'
-                }
-            ]
-        }
-    },
-    components: {
-        HelloWorld,
-        ToDo
-    },
-    methods: {
-        reverseMessage() {
-            this.msg = this.msg
-                .split('')
-                .reverse()
-                .join('')
-        }
-    }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
